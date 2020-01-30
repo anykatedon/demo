31c81059bfb3f4b4975425b11198e76210e6cc7f
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-01-30 12:22:53 +0530

    Added PostService.js with axios & updated Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,33 @@
 <template>
-  <h1>Home Page</h1>
+  <div class="row"></div>
 </template>
 
 <script>
-export default {
+import PostService from "../PostService";
+const postService = new PostService();
 
-}
+export default {
+  name: "Home",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  created() {
+    postService
+      .getAllPosts()
+      .then(res => {
+        this.posts = res.data;
+        // eslint-disable-next-line
+        console.log(this.posts);
+      })
+      .catch(err => {
+        // eslint-disable-next-line
+        console.error(err);
+      });
+  }
+};
 </script>
 
 <style>
-
 </style>
